c32a71c3888a3f2e7caefc38b9659953f9ffe4d1
Hugo A. Pantoja <hugoandres272@gmail.com>
2022-01-30 17:53:06 -0500

    Nuevos cambios a la página inicial y decimales

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -13,15 +13,21 @@ export class ToolbarComponent implements OnInit {
       color: `border-color: rgba(${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
                                   ${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
                                   ${Math.floor((Math.random() * (150 - 1 + 1)) + 1)},.5);`,
-      image: 'assets/images/5187699.jpg',
-      name: 'Nombre Completo Número Primero'
+      image: 'assets/images/lina.png',
+      name: 'Lina Maria Obando Guerrero',
+      title: 'Psicóloga, Universidad de Nariño',
+      profile: 'Magister en Psicodiagnóstico y Evaluación Psicológica, Universidad de Buenos Aires. Especialista en Psicología Forense y Criminal en curso, Universidad Konrad Lorenz. Miembro del Grupo de Investigación “Libres Pensadores” Categoria B de Colciencias.',
+      moreInfo: false
     },
     {
       color: `border-color: rgba(${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
                                   ${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
                                   ${Math.floor((Math.random() * (150 - 1 + 1)) + 1)},.5);`,
       image: 'assets/images/5187699.jpg',
-      name: 'Nombre Completos Número Segundo'
+      name: 'Nombre Completos Número Segundo',
+      title: 'Psicólogo',
+      profile: 'Otro',
+      moreInfo: false
     },
     
   ]
