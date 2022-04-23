10fa558e3018522377efb2ca5c1b10d2c45a510a
Hugo A. Pantoja <hugoandres272@gmail.com>
2022-04-23 00:20:33 -0500

    Añadiendo cambios página de inicio

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -19,23 +19,26 @@ export class ToolbarComponent implements OnInit {
       profile: 'Magister en Psicodiagnóstico y Evaluación Psicológica, Universidad de Buenos Aires. Especialista en Psicología Forense y Criminal en curso, Universidad Konrad Lorenz. Miembro del Grupo de Investigación “Libres Pensadores” Categoria B de Colciencias.',
       moreInfo: false
     },
-    {
-      color: `border-color: rgba(${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
-                                  ${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
-                                  ${Math.floor((Math.random() * (150 - 1 + 1)) + 1)},.5);`,
-      image: 'assets/images/5187699.jpg',
-      name: 'Nombre Completos Número Segundo',
-      title: 'Psicólogo',
-      profile: 'Otro',
-      moreInfo: false
-    },
-    
+    // {
+    //   color: `border-color: rgba(${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
+    //                               ${Math.floor((Math.random() * (255 - 1 + 1)) + 1)},
+    //                               ${Math.floor((Math.random() * (150 - 1 + 1)) + 1)},.5);`,
+    //   image: 'assets/images/5187699.jpg',
+    //   name: 'Nombre Completos Número Segundo',
+    //   title: 'Psicólogo',
+    //   profile: 'Otro',
+    //   moreInfo: false
+    // },
   ]
 
   constructor() { }
 
   ngOnInit(): void {
-    const nameVar = localStorage.getItem("name").split(" ")[0];
+    try {
+      var nameVar = localStorage.getItem("name").split(" ")[0];
+    } catch (error) {
+      
+    }
     if(nameVar){
       this.name = "¡Hola "+nameVar+"!";
     }
